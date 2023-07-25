1e9ac8f4e4fa7e880f049d56fbcbcbaf3b22428a
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-07-26 07:37:52 +0900

    REST APIでデータを取得

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  // useEffect(() => {
+  //   axios.get("http://localhost:3003/data").then((res) => {});
+  // }, []);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get("http://localhost:3003/data");
+      setData(res.data);
+    };
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data.map((data) => {
+        return (
+          <div key={data.id}>
+            <h2>Type of Food</h2>
+            <h3>{data.title}</h3>
+            <h3>{data.id}</h3>
+            <p>{data.type.join(",")}</p>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
